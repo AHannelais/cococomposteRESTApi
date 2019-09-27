5729666343b798126a117486ab4c2a86973b3170
AHannelais <antoine.hannelais@gmail.com>
2019-09-27 21:34:43 +0200

    v1 ready to deploy

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const articleRoutes = require("./api/routes/articles");
-const imageRoutes = require("./api/routes/images");
-const mongoose = require("mongoose");
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const articleRoutes = require('./api/routes/articles')
+const imageRoutes = require('./api/routes/images')
+const mongoose = require('mongoose')
 
-app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+pw = process.env.MONGO_ATLAS_PW
 
 mongoose.connect(
-  `mongodb+srv://cocomposte:${process.env.MONGO_ATLAS_PW}@cocomposte-rest-api-12djo.mongodb.net/test?retryWrites=true&w=majority`,
+  `mongodb+srv://cocomposte:cocomposte31@cocomposte-rest-api-12djo.mongodb.net/test?retryWrites=true&w=majority`,
 
   { useNewUrlParser: true, useUnifiedTopology: true }
   // { useMongoClient: true }
-);
+)
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, PATCH, POST, DELETE, GET");
-    return res.status(200).json({});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, PATCH, POST, DELETE, GET')
+    return res.status(200).json({})
   }
-  next();
-});
+  next()
+})
 
-app.use("/articles", articleRoutes);
-app.use("/images", imageRoutes);
+app.use('/articles', articleRoutes)
+app.use('/images', imageRoutes)
 app.use((req, res, next) => {
-  const error = new Error("Not Found");
-  error.status = 404;
-  next(error);
-});
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({ error: { message: error.message } });
-});
-module.exports = app;
+  res.status(error.status || 500)
+  res.json({ error: { message: error.message } })
+})
+module.exports = app
